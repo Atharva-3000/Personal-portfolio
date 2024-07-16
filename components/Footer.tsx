4d2f64c5d2c9678cbbead52326d93aea4007342a
Atharva <deshmukhsambhajirao9@gmail.com>
2024-07-16 18:38:47 +0530

    minor error fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <h1 className='heading lg:max-w-[45vw]'>
                     Ready to take <span className='text-purple'>your</span> digital presence to the next level?
                 </h1>
-                <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let's discuss how I can help you achieve your goal.</p>
+                <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goal.</p>
                 <a href="mailto:atharvadeshmukh.dev@gmail.com" target='_blank'>
                     <MagicButton2 title="Get in touch" icon={<FaLocationArrow />} position='right' />
                 </a>
@@ -23,7 +23,7 @@ const Footer = () => {
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) =>
                     (
-                        <a href={profile.link}>
+                        <a href={profile.link} key={profile.id}>
                         <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                             <img src={profile.img} alt={profile.img} width={20} height={20} />
                         </div>
